d828753fe8cb041f32c44daa0294d2d71b7d86fc
Arnav41 <arnav04jan@gmail.com>
2023-04-13 10:55:53 +0530

    changed the bulb and re-scripted the theme change

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 const bulb = document.getElementById('bulb');
 
+// switching to dark theme
 bulb.onclick = function(){
     document.body.classList.toggle("dark-theme")
+    if(document.body.classList.contains("dark-theme")){
+        bulb.src = "img/bulb-2.png"
+    }else{
+        bulb.src = "img/bulb-1.png"
+    }
 }
 
 
